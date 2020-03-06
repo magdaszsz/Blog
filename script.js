e4116f10dcafec10b2d4c55ed482437e4d83972a
Magda Sz <pesahson@gmail.com>
2020-03-06 16:50:10 +0100

    add eventListener for images

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-
-
-
 window.onscroll = function() {
     
   scrollFunction();
   scroll()};
-
-  
 
   function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.querySelector(".top").style.height = "10vh";
     document.querySelector(".top").style.fontSize = "15px";
     document.querySelector("#logo").style.fontSize = "20px";
-
-
-}  else {
+  }  else {
     document.querySelector(".top").style.height = "15vh";
     document.querySelector(".top").style.fontSize = "20px";
     document.querySelector("#logo").style.fontSize = "35px";
-}}
+  }}
 
 mybutton = document.getElementById("scrollBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-
 
 function scroll() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -34,9 +24,20 @@ function scroll() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
- 
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; 
 }
+
+const images = document.querySelectorAll('.image');
+images.forEach((el) => {
+  el.addEventListener('click', function(e){
+  e.target.classList.toggle('active');
+  document.addEventListener('click', function(e) {
+  if(el != e.target) {
+    el.classList.remove('active');
+  }
+});
+})
+});
 
