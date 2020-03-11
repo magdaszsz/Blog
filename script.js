17d97341170e342379724ad30468a0eaa0698b44
Magda Sz <pesahson@gmail.com>
2020-03-11 12:48:28 +0100

    add media queries/finish navigation

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 window.onscroll = function() {
-    
   scrollFunction();
-  scroll()};
+  scroll()
+};
 
   function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.querySelector(".top").style.height = "10vh";
-    document.querySelector(".top").style.fontSize = "15px";
-    document.querySelector("#logo").style.fontSize = "20px";
+    document.querySelector("nav").classList.add('small');
+    document.querySelector(".logo").classList.add('smallLogo');
+    document.querySelector(".mobile-logo").classList.add('smallLogo');
   }  else {
-    document.querySelector(".top").style.height = "15vh";
-    document.querySelector(".top").style.fontSize = "20px";
-    document.querySelector("#logo").style.fontSize = "35px";
-  }}
+    document.querySelector("nav").classList.remove('small');
+    document.querySelector(".logo").classList.remove('smallLogo');
+    document.querySelector(".mobile-logo").classList.remove('smallLogo');
+
+  }
+}
+
+/**************** SCROLL BUTTON ****************/
 
 mybutton = document.getElementById("scrollBtn");
 
@@ -24,20 +28,44 @@ function scroll() {
   }
 }
 
-
 function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
-const images = document.querySelectorAll('.image');
-images.forEach((el) => {
-  el.addEventListener('click', function(e){
-  e.target.classList.toggle('active');
-  document.addEventListener('click', function(e) {
-  if(el != e.target) {
-    el.classList.remove('active');
-  }
-});
-})
-});
+/****************** SLIDE IN MENU IN MOBILE ***********/
 
+const toggler = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.nav-list');
+toggler.addEventListener('click', function() {
+  mobileMenu.classList.toggle('show');
+})
+
+
+/*************** MODAL POP UP ***************/
+
+const images = document.querySelectorAll('.gallery img');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelectorAll('.close');
+
+for(let image of images) {
+  image.addEventListener('click', function(e) {
+    const source = e.target.src;
+    modal.querySelector('img').setAttribute('src', source);
+    overlay.classList.add('show');
+    modal.classList.add('show');
+    
+  });
+}
+
+overlay.addEventListener('click', function() {
+  modal.classList.remove('show');
+  overlay.classList.remove('show');
+})
+
+for(let x of close) {
+  x.addEventListener('click', function() {
+    modal.classList.remove('show');
+    overlay.classList.remove('show');
+  })
+}
